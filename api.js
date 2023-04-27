736b519f10868f4f6ddc058e068ff28cd1bfedd8
Rama49 <108133344+Rama49@users.noreply.github.com>
2023-04-27 15:31:32 +0000

    Add files via upload
    ajouter des fichier dictionnaire

--- api.js
+++ api.js
@@ -1,35 +1,72 @@
-let ville = 'Dakar';
-recevoirTemperature(ville);
+const wrapper = document.querySelector(".wrapper"),
+searchInput = wrapper.querySelector("input"),
+volume = wrapper.querySelector(".word i"),
+infoText = wrapper.querySelector(".info-text"),
+synonyms = wrapper.querySelector(".synonyms .list"),
+removeIcon = wrapper.querySelector(".search span");
+let audio;
 
-let button = document.querySelector('#changer');
-button.addEventListener('click', () => {
-  ville = prompt('Choisissez une ville :');
-  recevoirTemperature(ville);
-});
+function data(result, word){
+    if(result.title){
+        infoText.innerHTML = `Impossible de trouver le sens de <span>"${word}"</span>. S'il vous plaît, essayez de rechercher un autre mot.`;
+    }else{
+        wrapper.classList.add("active");
+        let definitions = result[0].meanings[0].definitions[0],
+        phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
+        document.querySelector(".word p").innerText = result[0].word;
+        document.querySelector(".word span").innerText = phontetics;
+        document.querySelector(".meaning span").innerText = definitions.definition;
+        document.querySelector(".example span").innerText = definitions.example;
+        audio = new Audio("https:" + result[0].phonetics[0].audio);
 
-function recevoirTemperature(ville) {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=eb3e55ca0093756f2541d5ad27c5021c&units=metric';
-  
-  let requete = new XMLHttpRequest();
-  requete.open('GET', url);
-  requete.responseType = 'json';
-  requete.send();
-
-  requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-      if (requete.status === 200) {
-        let reponse = requete.response;
-        let ville = reponse.name;
-        let temperature = reponse.main.temp;
-        document.querySelector('#ville').textContent = ville;
-        document.querySelector('#temperature_label').textContent = temperature;
-      } else {
-        alert('Un problème est intervenu, merci de revenir plus tard.');
-      }
+        if(definitions.synonyms[0] == undefined){
+            synonyms.parentElement.style.display = "none";
+        }else{
+            synonyms.parentElement.style.display = "block";
+            synonyms.innerHTML = "";
+            for (let i = 0; i < 5; i++) {
+                let tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[i]},</span>`;
+                tag = i == 4 ? tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[4]}</span>` : tag;
+                synonyms.insertAdjacentHTML("beforeend", tag);
+            }
+        }
     }
-  }
 }
 
+function search(word){
+    fetchApi(word);
+    searchInput.value = word;
+}
 
-// let affichage = document.getElementById('affiche');'
-//     affichage.innerHTML = ville + temperature;
+function fetchApi(word){
+    wrapper.classList.remove("active");
+    infoText.style.color = "#000";
+    infoText.innerHTML = `Chercher le sens de <span>"${word}"</span>`;
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() =>{
+        infoText.innerHTML = `Impossible de trouver le sens de <span>"${word}"</span>. S'il vous plaît, essayez de rechercher un autre mot.`;
+    });
+}
+
+searchInput.addEventListener("keyup", e =>{
+    let word = e.target.value.replace(/\s+/g, ' ');
+    if(e.key == "Enter" && word){
+        fetchApi(word);
+    }
+});
+
+volume.addEventListener("click", ()=>{
+    volume.style.color = "#4D59FB";
+    audio.play();
+    setTimeout(() =>{
+        volume.style.color = "#999";
+    }, 800);
+});
+
+removeIcon.addEventListener("click", ()=>{
+    searchInput.value = "";
+    searchInput.focus();
+    wrapper.classList.remove("active");
+    infoText.style.color = "#9A9A9A";
+    infoText.innerHTML = "Tapez n'importe quel mot existant et appuyez sur Entrée pour obtenir une signification, des synonymes ...";
+});
